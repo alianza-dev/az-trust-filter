79fa6925f2b4945ef8f26402570d47b10749d3f5
Kent C. Dodds <kent@doddsfamily.us>
2015-08-03 12:54:06 -0600

    1.0.0

---
+++ dist/az-trust-filter.js
@@ -0,0 +1,108 @@
+//! az-trust-filter version 1.0.0 built with ♥ by Kent C. Dodds <kent@doddsfamily.us> (http://kentcdodds.com) (ó ì_í)=óò=(ì_í ò)
+
+(function webpackUniversalModuleDefinition(root, factory) {
+	if(typeof exports === 'object' && typeof module === 'object')
+		module.exports = factory(require("angular"), require("'ngSanitize'"));
+	else if(typeof define === 'function' && define.amd)
+		define(["angular", "'ngSanitize'"], factory);
+	else if(typeof exports === 'object')
+		exports["azTrustFilter"] = factory(require("angular"), require("'ngSanitize'"));
+	else
+		root["azTrustFilter"] = factory(root["angular"], root["'ngSanitize'"]);
+})(this, function(__WEBPACK_EXTERNAL_MODULE_1__, __WEBPACK_EXTERNAL_MODULE_2__) {
+return /******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId])
+/******/ 			return installedModules[moduleId].exports;
+
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			exports: {},
+/******/ 			id: moduleId,
+/******/ 			loaded: false
+/******/ 		};
+
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+
+/******/ 		// Flag the module as loaded
+/******/ 		module.loaded = true;
+
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+
+
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(0);
+/******/ })
+/************************************************************************/
+/******/ ([
+/* 0 */
+/***/ function(module, exports, __webpack_require__) {
+
+	'use strict';
+
+	Object.defineProperty(exports, '__esModule', {
+	  value: true
+	});
+
+	function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { 'default': obj }; }
+
+	var _angular = __webpack_require__(1);
+
+	var _angular2 = _interopRequireDefault(_angular);
+
+	__webpack_require__(2);
+
+	var ngModuleName = 'azTrustFilter';
+	var ngModule = _angular2['default'].module(ngModuleName, ['ngSanitize']);
+
+	exports['default'] = ngModule.filter('trust', trustFunction).name;
+	// <-- exporting the module name
+
+	function trustFunction($sce) {
+	  return trust;
+
+	  function trust(value, type) {
+	    if (value) {
+	      return $sce.trustAs(type || 'html', value);
+	    } else {
+	      return value;
+	    }
+	  }
+	}
+	trustFunction.$inject = ["$sce"];
+	module.exports = exports['default'];
+
+/***/ },
+/* 1 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_1__;
+
+/***/ },
+/* 2 */
+/***/ function(module, exports) {
+
+	module.exports = __WEBPACK_EXTERNAL_MODULE_2__;
+
+/***/ }
+/******/ ])
+});
+;
